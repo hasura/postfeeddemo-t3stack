212971b0198f979f9ecf86bfca65f0c10cb93767
Akshaya Acharya <akshaya.acharya.01@gmail.com>
2023-06-20 18:58:49 +0530

    Import seed data for likes, comments, comments likes

--- bin/loadSeedData.sql
+++ bin/loadSeedData.sql
@@ -1,2 +1,7 @@
 copy "User"(id, name, "createdAt") from '/users.csv' csv header;
 copy "Post"(id, "userId", title, body, starred, "createdAt") from '/posts.csv' csv header; 
+
+copy "Like"(id, "userId", "postId") from '/likes.csv' csv header;
+
+copy "Comment"(id, "postId", body, "createdAt") from '/comments.csv' csv header;
+copy "CommentLike"(id, "userId", "commentId") from '/comments_likes.csv' csv header;
---
+++ data/import-data.sh
@@ -0,0 +1,5 @@
+docker compose cp data/users.csv database:/
+docker compose cp data/posts.csv database:/
+docker compose cp data/likes.csv database:/
+docker compose cp data/comments.csv database:/
+docker compose cp data/comments_likes.csv database:/
